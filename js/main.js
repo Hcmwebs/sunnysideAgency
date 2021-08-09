f2c67403f89d14d3af9b4e318a140fecabc92933
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-08-09 14:05:19 +1200

    added active class functionality
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -1,31 +1,23 @@
 const hamburger = document.getElementById('hamburger');
 const navBar = document.getElementById('navbar');
-const navLinks = navBar.querySelectorAll('li a');
+const navLinks = navBar.querySelectorAll('li > a');
 
-
-
-
-
- // === === opening the navBar
+// === === opening the navBar
 hamburger.addEventListener('click', () => {
   hamburger.classList.add('open');
   navbar.classList.add('open');
-})
+});
 
 // === === closing the navBar
 navBar.addEventListener('click', () => {
   hamburger.classList.remove('open');
   navBar.classList.remove('open');
-})
-
+});
 
 // === === adding class active to the nav links
-const linkClicked = (e) => {
-  navLinks.forEach(link => link.classList.remove('active'))
-  navLinks.forEach(link => e.target.classList.add('active'))
-};
-
-navLinks.forEach(link => link.addEventListener('click', linkClicked));
-
-
-
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navBar.querySelector('.active').classList.remove('active');
+    link.classList.add('active');
+  });
+});
